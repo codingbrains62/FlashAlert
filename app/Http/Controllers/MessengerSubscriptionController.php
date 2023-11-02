b9678ec3bbf5d2586caece2ba4329a9f4c1f8c96
codingbrains62 <codingbrains62@gmail.com>
2023-11-02 15:28:36 +0530

    working on messenger sub login.

--- app/Http/Controllers/MessengerSubscriptionController.php
+++ app/Http/Controllers/MessengerSubscriptionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MessengerSubscriptionController extends Controller
 {
@@ -15,7 +16,31 @@ class MessengerSubscriptionController extends Controller
     public function attach_app(){
         return view('frontend.attachAppTut');
     }
-    public function frontend_region(){
-        return view('frontend.regionsForMsgLogin');
+    public function frontend_region(Request $request){
+        $data['regionlist'] = DB::table('regions')->get();
+
+        
+        if ($request->has('search')) {
+            $regionID = $request->input('RegionID');
+            $organizationName = $request->input('organizationName');
+            // Construct the query for searching organizations based on the selected region and organization name
+            $query = DB::table('orgs')
+            ->join('regions', 'orgs.RegionID', '=', 'regions.id')// Add a join with the 'regions' table
+            ->join('users', 'orgs.id', '=', 'users.OrgID') // Add a join with the 'users' table
+            ->select('orgs.*', 'regions.Description as regionDescription', 'users.URLName as URLName');
+            if ($regionID != 0) {
+                $query->where('regions.id', $regionID);
+            }
+            if (!empty($organizationName)) {
+                $query->where('orgs.Name', 'like', '%' . $organizationName . '%');
+            }
+        //Execute the query and fetch search results
+         $data['searchResults'] = $query->get();
+            // echo"<pre>";
+            // print_r($data['searchResults']);
+            // die;
+        }
+        return view('frontend.regionsForMsgLogin', $data);
+
     }
 }
